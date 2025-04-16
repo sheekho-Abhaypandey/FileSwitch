13f5ce13e16fe830fcba746bb518657f51401ee8
Abhay Pandey <abhaypandeyaman@gmail.com>
2025-04-16 13:24:45 +0530

    Implement theme management in App component with light and dark mode support

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,36 @@
 import Navbar from './components/Navbar.jsx';
 import Home from './components/Home.jsx';
 import Footer from './components/Footer.jsx';
+import React, { useState, useEffect } from "react";
+// import { useEffect, useState } from "react"
+import { ThemeProvider } from "./contexts/theme.js";
 
 
 function App() {
 
+  const [themeMode,setThemeMode]=useState("light");
+
+  const lightTheme=()=>{
+    setThemeMode("light");
+  }
+
+  const darkTheme=()=>{
+    setThemeMode("dark");
+  }
+
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light", "dark");
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+
   return (
-    <>
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
       <Navbar />
       <Home />
       <Footer />
      
-    </>
+    </ThemeProvider>
   )
 }
 
