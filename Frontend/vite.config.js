7bab9425dd90cede45b379ad9cbef38f9110baf5
Abhay Pandey <abhaypandeyaman@gmail.com>
2025-04-16 13:25:21 +0530

    Add server proxy configuration for file conversion

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/convertFile':'http://localhost:3001',
+    }
+  },
   plugins: [react(), tailwindcss(),],
 })
