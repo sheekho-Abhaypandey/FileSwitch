ba8a8fd3481f6bc2884e9eae3abba401ed56ff7b
Abhay Pandey <abhaypandeyaman@gmail.com>
2025-04-16 13:21:25 +0530

    Refactor Navbar component to integrate theme management and improve structure

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,40 +1,46 @@
 
-import React, { useState, useEffect } from "react";
-// import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react"
+import { ThemeProvider } from "../contexts/theme.js";
+import ThemeBtn from "../components/Themebtn";
 
 function Navbar() {
-//   const navigate = useNavigate();
-//   const [user, setUser] = useState(null);
-//   const [dropdownOpen, setDropdownOpen] = useState(false); 
+  const [themeMode,setThemeMode]=useState("light");
 
-//   useEffect(() => {
-//     const storedUser = JSON.parse(localStorage.getItem("user"));
-//     if (storedUser) {
-//       setUser(storedUser); // ✅ Set logged-in user
-//     }
-//   }, []);
+  const lightTheme=()=>{
+    setThemeMode("light");
+  }
 
-//   const handleLogout = () => {
-//     localStorage.removeItem("user");
-//     localStorage.removeItem("token");
-//     setUser(null);
-//     navigate("/");
-//   };
+  const darkTheme=()=>{
+    setThemeMode("dark");
+  }
+
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light", "dark");
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
 
   return (
-    <div className="max-w-screen-2xl mx-auto container px-6 py-3 md:px-40 shadow-lg h-16 fixed border-b-2 bg-white">
-      <div className="flex justify-between">
-        <h1 className="text-2xl cursor-pointer font-bold text-white">
-          Word<span className="text-3xl text-green-500">To</span>PDF
+         <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+    
+    <div className="max-w-screen-2xl mx-auto container px-6 py-3 md:px-40 shadow-lg h-16 fixed border bg-white text-black dark:bg-gray-900 dark:text-white">
+
+      <div className="flex justify-between ">
+        <h1 className="text-2xl cursor-pointer font-bold text-black dark:text-white dark:bg-gray-900">
+          Word<span className="text-3xl  text-green-500">To</span>PDF
         </h1>
-        <div className="flex gap-4">
-          <h1 className="mt-1 text-2xl cursor-pointer font-bold hover:scale-125 duration-300 text-black">
+        <div className="flex gap-4 items-center">
+          <h1 className=" text-2xl cursor-pointer font-bold hover:scale-125 duration-300 text-black dark:text-white">
             Home
           </h1>
+
+            <ThemeBtn/>
+         
 
          </div>
       </div>
     </div>
+    </ThemeProvider>
   );
 }
 
