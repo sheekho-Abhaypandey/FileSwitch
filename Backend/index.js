aedfdca43d1102302e8935142aa1c8053ed0371b
Abhay Pandey <abhaypandeyaman@gmail.com>
2025-05-11 21:57:12 +0530

    Backend code

--- Backend/index.js
+++ Backend/index.js
@@ -5,10 +5,17 @@ const path=require('path');
 const cors=require('cors');
 const axios=require('axios');
 const  cleanFolder=require('./utils/CleanUploadFolder');
+const archiver = require('archiver');
+const CloudConvert =require('cloudconvert');
 
-const CLOUDCONVERT_API_KEY='eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZjg5MjljODI2NDYxZmQ5NTBlZTI3YjhkM2U1MWU3ODJjMmQzOGJmMzkxYmQxOWFkNDgwMWUwNzY1MGJhYWIzOTk2NmU5NGE0Y2QyMDM5OGEiLCJpYXQiOjE3NDAxMzM5ODcuOTc5MzQsIm5iZiI6MTc0MDEzMzk4Ny45NzkzNDEsImV4cCI6NDg5NTgwNzU4Ny45NzUyMzIsInN1YiI6IjY4MjI2MzUxIiwic2NvcGVzIjpbInVzZXIucmVhZCIsInVzZXIud3JpdGUiLCJ0YXNrLnJlYWQiLCJ0YXNrLndyaXRlIiwid2ViaG9vay5yZWFkIiwid2ViaG9vay53cml0ZSIsInByZXNldC5yZWFkIiwicHJlc2V0LndyaXRlIl19.r3hiNMwdi2TeE9dc2MIn-mDtwwEnRm1hpyRMjip9DHH_sD9YApcA-uP_FPaoDFfxc8jihFgp69e3Pl-DxVw_7ugqVU2_FdOsyF26ZL3ytb7jG28BuEBza3anESK-Qy_Y1MNtVvvtoFJ4OYz4QgDvPGKMpiLjpcRHWbOXnaVZa4csPx9NM6hp5SIjOH38434tWLlIuI9CSeHrcyCd1QxM4SQwveg2hEGZm43yw7EoOtjs4ea2OVwmUZh9DE0Dr2pUyHzANR-ARrKkbBYaBGspwKLAPAVrz6Cu3JW1eX1NpNR0HlcVrW2sgxCuaBx2KmW4NW8Zipd49Bqq1YtiCCpUR_xXxdm9Q01WN6tqcD60IQkDH9ueCkLHHe9UjHmFVz0fQ-73LMQ7LnSnsLrMPNsLKbRUsAl_kGGcPdjDJ8n84PyIrassi0qfHFQUDKQv3_OV7aC9UOYubgKxpueDQ0U02bBhBbA5LghRHIXUk3haiSU1DCTVgSsFa-VLKOu0oSlcQXPE9DXf0sd2TpIexPPlWmnJchL9_2PrU9v2uMX9osF2CEueB3HP_7NYQTHqA2vL2uxx5j_iFwPnWWIW8Uj-BkV0N9hPvwu8DaCgscvZ-U3b7xO4cREhCFTsxNyD_LL3NBNi7XcF7myrNF_1hoMoYBVBYwWbRkTQITffItlM_eo';
+const router = express.Router();
+const cloudConvert = new CloudConvert('eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZjQ2NzI2MjQwOGQ1NThjNDI5OWVjZjZmM2NkYzIzM2FhOTMxNjlmNWZhYmM5OTY5NmEwMzA0NWFkNTUzOWNkNDJhZjM2NTdhOGJkZGVhZDgiLCJpYXQiOjE3NDY5NDY1MjUuNTE5ODA5LCJuYmYiOjE3NDY5NDY1MjUuNTE5ODEyLCJleHAiOjQ5MDI2MjAxMjUuNTE2MDc2LCJzdWIiOiI2ODIyNjM1MSIsInNjb3BlcyI6WyJ1c2VyLnJlYWQiLCJ1c2VyLndyaXRlIiwidGFzay5yZWFkIiwidGFzay53cml0ZSIsIndlYmhvb2sucmVhZCIsIndlYmhvb2sud3JpdGUiLCJwcmVzZXQucmVhZCIsInByZXNldC53cml0ZSJdfQ.pQYXRy7-5Nl_qZ_CnKyAMupdvCLoRyIzv8QQHcq-8nUXXxUXXDrXkaO-bj-1WKmYzLrFTb7TR6oVHBBt57cGdZJ0MplYgptzu116MFMQYmMrfSyNYibKHGWTKRHndBaOP3ag7fs_au5vQtZJLcnzixN2fDHPGCMq8lIeX-0iPDabhEqrx2JioUPORAdYLYaFTdIhCU2aWjznjTisUeB0rFGfNZzJKM92nWuFeHu61oYifPDZUxrLPeDqpO7URncuC_9uZfD-BrmIsirvKYdPg-htKaPDc0h5F1_6e7L1FhCRxlgJ0pl3EVk0s2vgDDgjLIo_VeD7mduhtGaHeCQaRubr1YfKWeRV6ZQ5LoiHVVMyeACaEfiLM4BvLRGYT34uB7TQPcXtM-_pt-9m4M4zQ8a65YjeW-kWZeqZLHdkWNhpK3WpZ--5qTBLhm9I0aw8oKT-vlIOvbiPc-cLitX150-pkSix-WJv7M9mGCqzMMSFa52-VRk9onptOgZeZWlKc5JofKZS28JBqgQaAUtPHBhKJXidvB7on_8EJb7U0fXFm4geAFsB1CqPjjtFMmk1DYcbiQ0N-Xr-hvs7KIjM8qU8OJkijp98r-kZXtp5X2vDfguTU7oc2lG_GHP_nGvZFlbSifEaWGP7OfQWFTu59UUvqZtWBiRWBq0SiPq6MPw');
+
+
+// const CLOUD_CONVERT_APIKEY="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZjQ2NzI2MjQwOGQ1NThjNDI5OWVjZjZmM2NkYzIzM2FhOTMxNjlmNWZhYmM5OTY5NmEwMzA0NWFkNTUzOWNkNDJhZjM2NTdhOGJkZGVhZDgiLCJpYXQiOjE3NDY5NDY1MjUuNTE5ODA5LCJuYmYiOjE3NDY5NDY1MjUuNTE5ODEyLCJleHAiOjQ5MDI2MjAxMjUuNTE2MDc2LCJzdWIiOiI2ODIyNjM1MSIsInNjb3BlcyI6WyJ1c2VyLnJlYWQiLCJ1c2VyLndyaXRlIiwidGFzay5yZWFkIiwidGFzay53cml0ZSIsIndlYmhvb2sucmVhZCIsIndlYmhvb2sud3JpdGUiLCJwcmVzZXQucmVhZCIsInByZXNldC53cml0ZSJdfQ.pQYXRy7-5Nl_qZ_CnKyAMupdvCLoRyIzv8QQHcq-8nUXXxUXXDrXkaO-bj-1WKmYzLrFTb7TR6oVHBBt57cGdZJ0MplYgptzu116MFMQYmMrfSyNYibKHGWTKRHndBaOP3ag7fs_au5vQtZJLcnzixN2fDHPGCMq8lIeX-0iPDabhEqrx2JioUPORAdYLYaFTdIhCU2aWjznjTisUeB0rFGfNZzJKM92nWuFeHu61oYifPDZUxrLPeDqpO7URncuC_9uZfD-BrmIsirvKYdPg-htKaPDc0h5F1_6e7L1FhCRxlgJ0pl3EVk0s2vgDDgjLIo_VeD7mduhtGaHeCQaRubr1YfKWeRV6ZQ5LoiHVVMyeACaEfiLM4BvLRGYT34uB7TQPcXtM-_pt-9m4M4zQ8a65YjeW-kWZeqZLHdkWNhpK3WpZ--5qTBLhm9I0aw8oKT-vlIOvbiPc-cLitX150-pkSix-WJv7M9mGCqzMMSFa52-VRk9onptOgZeZWlKc5JofKZS28JBqgQaAUtPHBhKJXidvB7on_8EJb7U0fXFm4geAFsB1CqPjjtFMmk1DYcbiQ0N-Xr-hvs7KIjM8qU8OJkijp98r-kZXtp5X2vDfguTU7oc2lG_GHP_nGvZFlbSifEaWGP7OfQWFTu59UUvqZtWBiRWBq0SiPq6MPw";
+
 app.use(cors());
-const port=3001;
+const port=3001|| process.env.PORT;
 const fs = require('fs');
 require('dotenv').config();
 
@@ -23,6 +30,7 @@ const FormData = require("form-data");
 
    
     const storage = multer.diskStorage({
+        limits: { fileSize: 2 * 1024 * 1024 },
         destination: "uploads",
         filename: (req, file, cb) => {
           cb(null, file.originalname);  // Keep the original filename
@@ -62,9 +70,10 @@ const FormData = require("form-data");
         console.log("error occurred",err);
     }
   
- })
+ });
 
- app.post('/convertFile/docx-to-jpg', uploads.single("file"), async (req, res,next) => {
+ 
+  app.post('/convertFile/docx-to-jpg', uploads.single("file"), async (req, res,next) => {
     try {
         if (!req.file) {
             return res.status(400).json({ message: "No file uploaded" });
@@ -92,6 +101,8 @@ const FormData = require("form-data");
         res.status(500).json({ message: "Internal server error" });
     }
 });
+
+
 
 app.post('/convertFile/docx-to-png', uploads.single("file"), async (req, res) => {
     try {
@@ -287,6 +298,7 @@ app.post('/convertFile/jpg-to-pdf', uploads.single('file'), async(req,res,next)=
 
    
       const result= await convertapi.convert('pdf', {File:jpgfile}, 'jpg');
+      console.log(result);
         await result.saveFiles(outputfile);
         res.download(outputfile,(err)=>{
             if(err){
