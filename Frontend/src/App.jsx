bbb2a657cec06d0a305accedeef8dc727ab9b00d
Abhay Pandey <abhaypandeyaman@gmail.com>
2025-05-11 22:58:02 +0530

    frontend code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,37 +1,35 @@
+import React, { useState, useEffect } from "react";
 import Navbar from './components/Navbar.jsx';
 import Home from './components/Home.jsx';
+import Contact from './components/Contact.jsx';
+import About from './components/About.jsx';
 import Footer from './components/Footer.jsx';
-import React, { useState, useEffect } from "react";
-// import { useEffect, useState } from "react"
+
 import { ThemeProvider } from "./contexts/theme.js";
 
-
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+  const [page, setPage] = useState("home"); // <-- New state
 
-  const [themeMode,setThemeMode]=useState("light");
+  const lightTheme = () => setThemeMode("light");
+  const darkTheme = () => setThemeMode("dark");
 
-  const lightTheme=()=>{
-    setThemeMode("light");
-  }
-
-  const darkTheme=()=>{
-    setThemeMode("dark");
-  }
-
-
-  useEffect(()=>{
+  useEffect(() => {
     document.querySelector('html').classList.remove("light", "dark");
-    document.querySelector('html').classList.add(themeMode)
-  },[themeMode])
+    document.querySelector('html').classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
-      <Navbar />
-      <Home />
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <Navbar setPage={setPage} />
+      
+      {page === "home" && <Home />};
+      {page === "contact" && <Contact />};
+      {page === "about" && <About />};
+
       <Footer />
-     
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
